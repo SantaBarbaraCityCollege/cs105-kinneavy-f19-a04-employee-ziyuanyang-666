622e23925cac7e8786602a3374877514c22dd36f
James Kinneavy <jskproxy@outlook.com>
2019-09-12 20:19:56 -0700

    Initial commit

---
+++ src/main/java/edu/sbcc/cs105/Program.java
@@ -0,0 +1,10 @@
+// Replace all code in this file with what is provided in the assignment
+package edu.sbcc.cs105;
+
+public class Program {
+
+    public static void main(String[] args) {
+        System.out.println("Someday, I'll be a real program!");
+    }
+
+}
